e64e21f462d971243eca66cf763394c76fe34382
Aakarsh Godhar <aakarshgodhar01@gmail.com>
2023-06-23 12:27:33 +0530

    Initial Commit

---
+++ 1.py
@@ -0,0 +1,13 @@
+# list_0 = ["Aakarsh","Goku",0,8,8,8,8,8,8,8,8,8,8]
+# tuple_0 = ("Aakarsh","Goku",0,9,8,7,6,5,4,3,2,1)
+# dict_0 = {"AAAPPPLLLEEE":"Apple","TATTTIII":"Potty"}
+# print(list_0)
+# print(tuple_0)
+# print(dict_0)
+
+str = "Aakarsh loves Shivi"
+bstr = bytes(str,'utf-16')
+print(bstr)
+
+bytestr = bytes(4)
+print(bytestr)
